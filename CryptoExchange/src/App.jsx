88fe625b69da6dd41f90654f9e2fcf6b60d72994
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-13 12:00:27 +0500

    Set Up enviroment for crypto verse web app

---
+++ CryptoExchange/src/App.jsx
@@ -0,0 +1,14 @@
+import { Link, Switch, Route } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+function App() {
+	return (
+		<div className="app">
+			<div className="navbar"></div>
+			<div className="main"></div>
+			<div className="footer"></div>
+		</div>
+	);
+}
+
+export default App;
