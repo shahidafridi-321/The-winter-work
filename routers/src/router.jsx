7b5bb13fa70a20c971e94f1394e7f6154392e9fc
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-13 19:46:34 +0500

    Create Error route

--- routers/src/router.jsx
+++ routers/src/router.jsx
@@ -3,10 +3,12 @@ import { Mainlayout } from "./MultiPages/Mainlayout";
 import { Home } from "./MultiPages/Home";
 import { About } from "./MultiPages/About";
 import { Notes } from "./MultiPages/Notes";
+import { Errorpage } from "./MultiPages/Errorpage";
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Mainlayout />,
+		errorElement: <Errorpage />,
 		children: [
 			{
 				path: "/",
@@ -17,8 +19,14 @@ export const router = createBrowserRouter([
 				element: <About />,
 			},
 			{
-				path: "/notes/:name",
+				path: "/notes",
 				element: <Notes />,
+				children: [
+					{
+						path: "/notes/:name",
+						element: <Notes />,
+					},
+				],
 			},
 		],
 	},
