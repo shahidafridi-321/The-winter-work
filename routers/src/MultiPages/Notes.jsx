2937bcd878c60263237d072734726861c614fe8c
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-13 19:48:28 +0500

    If no id is passed in url all the items is rendered

--- routers/src/MultiPages/Notes.jsx
+++ routers/src/MultiPages/Notes.jsx
@@ -3,12 +3,17 @@ import { notes } from "../notes";
 import { useParams } from "react-router-dom";
 export const Notes = () => {
 	const { name } = useParams();
+
 	const note = notes.find((note) => note.id == name);
 
 	return (
 		<div>
 			<ul>
-				<li>{note.content}</li>
+				{!name || name == "all" ? (
+					notes.map((note) => <li key={note.id}>{note.content}</li>)
+				) : (
+					<li>{note.content}</li>
+				)}
 			</ul>
 		</div>
 	);
