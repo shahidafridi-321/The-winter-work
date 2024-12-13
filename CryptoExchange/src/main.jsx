814157c278f2f931001528ade513e9e5b70eca33
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-13 19:28:29 +0500

    Creat About compoent

--- CryptoExchange/src/main.jsx
+++ CryptoExchange/src/main.jsx
@@ -1,42 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 
-import { Homepage } from "./components/Homepage";
-import { Exchanges } from "./components/Exchanges";
-import { Cryptocurrencies } from "./components/Cryptocurrencies";
-import { CryptoDetails } from "./components/CryptoDetails";
-import { News } from "./components/News";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./MultiPageSite/routes";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Homepage />,
-	},
-	{
-		path: "/exchanges",
-		element: <Exchanges />,
-	},
-	{
-		path: "/cryptocurrencies",
-		element: <Cryptocurrencies />,
-	},
 
-	{
-		path: "/crpto/:coinId",
-		element: <CryptoDetails />,
-	},
-	{
-		path: "/news",
-		element: <News />,
-	},
-]);
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<RouterProvider router={router} />
-		<App />
 	</StrictMode>
 );
--- routers/src/main.jsx
+++ routers/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Display } from "./Display";
-import Phonebook from "./PhoneBook";
-import { Image } from "./Image";
-import Profile from "./Profle";
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Profile  delay={1000}/>
+		<RouterProvider router={router} />
 	</StrictMode>
 );
