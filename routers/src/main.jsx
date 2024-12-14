9db4ce45058b794a7052ea3c8b47d890645f7040
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-14 09:44:56 +0500

    Display phonebook component

--- routers/src/main.jsx
+++ routers/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { phonebookRouter } from "./phonebook/PhonebookRoutes";
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<RouterProvider router={phonebookRouter} />
 	</StrictMode>
 );
