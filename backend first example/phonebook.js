8bc244b0ac85f0c694acc3f9a15e84a370f0d2dc
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-15 21:09:29 +0500

    Create and add a route api/info using express that response with a message to display

---
+++ backend first example/phonebook.js
@@ -0,0 +1,43 @@
+const http = require("http");
+
+const phonebook = [
+	{
+		id: "1",
+		name: "Arto Hellas",
+		number: "040-123456",
+	},
+	{
+		id: "2",
+		name: "Ada Lovelace",
+		number: "39-44-5323523",
+	},
+	{
+		id: "3",
+		name: "Dan Abramov",
+		number: "12-43-234345",
+	},
+	{
+		id: "4",
+		name: "Mary Poppendieck",
+		number: "39-23-6423122",
+	},
+];
+/* const app = http.createServer((request, response) => {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	response.end(JSON.stringify(phonebook));
+}); */
+
+const express = require("express");
+const app = express();
+
+app.get("/api/info/", (request, response) => {
+	response.send(
+		`<p>Phone book has info for ${
+			phonebook.length
+		} people</p><p>${new Date()}</p>`
+	);
+});
+
+const PORT = 3006;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
