af7348fec4b073fea25a1a42aa679440e631a42b
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-15 20:39:14 +0500

    Create a backend with express that handles get,delete and post requests

--- backend first example/index.js
+++ backend first example/index.js
@@ -18,7 +18,7 @@ let notes = [
 	},
 ];
 
-const app = http.createServer((request, response) => {
+/* const app = http.createServer((request, response) => {
 	response.writeHead(200, { "Content-Type": "application/json" });
 	response.end(JSON.stringify(notes));
 });
@@ -26,15 +26,60 @@ const app = http.createServer((request, response) => {
 const PORT = 3005;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
+ */
 
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (request, response) => {
-	response.send("<h1>Hellod world</h1>");
+	response.send("<h1>Hello world</h1>");
 });
-
 app.get("/api/notes", (request, response) => {
 	response.json(notes);
 });
+
+app.get("/api/notes/:id", (request, response) => {
+	const id = request.params.id;
+	const note = notes.find((note) => note.id === id);
+
+	note ? response.json(note) : response.status(404).end();
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+	const id = request.params.id;
+	notes = notes.filter((note) => note.id !== id);
+	response.status(204).end();
+});
+
+const generateId = () => {
+	const maxId =
+		notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+	return String(maxId + 1);
+};
+
+app.post("/api/notes/", (request, response) => {
+	const body = request.body;
+	if (!body.content) {
+		return response.status(400).json({
+			error: "content is missing",
+		});
+	}
+
+	const note = {
+		content: body.content,
+		important: Boolean(body.important) || false,
+		id: generateId(),
+	};
+
+	notes = notes.concat(note);
+	response.json(note);
+});
+
+const PORT = 3005;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
+
+
